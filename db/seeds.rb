c4fdfb4c4cbf99370e143fcc0d6e6d29ef1bdd7b
salinn <pmd6624@rit.edu>
2015-10-15 16:44:20 -0400

    added user course realtionship and seeds for these files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#No Dependincies
 load 'db/seeds/users.rb'
 load 'db/seeds/words.rb'
 load 'db/seeds/word_roots.rb'
-load 'db/seeds/word_roots_words.rb'
+load 'db/seeds/courses.rb'
+
+#Dependincies <-- Order matters
+load 'db/seeds/courses_users.rb' #Depends on Courses and Users
+load 'db/seeds/word_roots_words.rb' #Depends on words and word roots
