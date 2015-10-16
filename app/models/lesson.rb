1b66d0865ab88d864e12d8015d7014b2a23dbb2a
salinn <pmd6624@rit.edu>
2015-10-15 23:47:34 -0400

    added validations

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,10 @@
 class Lesson < ActiveRecord::Base
   belongs_to :course
+
+  validates :lesson_name, length: { in: 3..100 }
+  validates :lesson_points, inclusion: 0..100
+  validates :penalty, inclusion: 0..100
+  validates :course, presence: true
+  validates :lesson_start_time, presence: true
+  validates :lesson_end_date, presence: true
 end
