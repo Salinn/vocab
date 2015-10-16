c07f6bb42394da073e902cb31ba5666a271eef3b
salinn <pmd6624@rit.edu>
2015-10-16 01:36:20 -0400

    updated views and controller for definition

--- db/migrate/20151013171509_create_join_table_word_roots_words.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableWordRootsWords < ActiveRecord::Migration
-  def change
-    create_join_table :words, :word_roots do |t|
-      t.index [:word_id, :word_root_id]
-      t.index [:word_root_id, :word_id]
-    end
-  end
-end
