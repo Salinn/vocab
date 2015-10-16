c82707d17990dcf4b6cbd0e79c857d4c6c9f92f5
salinn <pmd6624@rit.edu>
2015-10-16 01:11:28 -0400

    updated migration table to where it should be. added seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016050024) do
+ActiveRecord::Schema.define(version: 20151016050821) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "class_name", limit: 255
