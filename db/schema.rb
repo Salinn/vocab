befc050053f0ddf80d22d7bb481fa35155d3221b
Nathan Perry <nbp8557@rit.edu>
2015-10-20 23:44:43 -0400

    Fixing schema DB file

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
+
 ActiveRecord::Schema.define(version: 20151020162554) do
-=======
-ActiveRecord::Schema.define(version: 20151016044710) do
->>>>>>> Definitions
 
   create_table "courses", force: :cascade do |t|
     t.string   "class_name", limit: 255
@@ -33,7 +30,7 @@ ActiveRecord::Schema.define(version: 20151016044710) do
   add_index "courses_users", ["course_id", "user_id"], name: "index_courses_users_on_course_id_and_user_id", using: :btree
   add_index "courses_users", ["user_id", "course_id"], name: "index_courses_users_on_user_id_and_course_id", using: :btree
 
-<<<<<<< HEAD
+
   create_table "lessons", force: :cascade do |t|
     t.string   "lesson_name",       limit: 255
     t.integer  "lesson_points",     limit: 4
@@ -46,7 +43,7 @@ ActiveRecord::Schema.define(version: 20151016044710) do
   end
 
   add_index "lessons", ["course_id"], name: "index_lessons_on_course_id", using: :btree
-=======
+
   create_table "definitions", force: :cascade do |t|
     t.text     "word_definition", limit: 65535
     t.datetime "created_at",                    null: false
@@ -55,7 +52,7 @@ ActiveRecord::Schema.define(version: 20151016044710) do
   end
 
   add_index "definitions", ["word_id"], name: "index_definitions_on_word_id", using: :btree
->>>>>>> Definitions
+
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -117,11 +114,10 @@ ActiveRecord::Schema.define(version: 20151016044710) do
     t.datetime "updated_at",             null: false
   end
 
-<<<<<<< HEAD
+
   add_foreign_key "lessons", "courses"
   add_foreign_key "word_forms", "words"
   add_foreign_key "word_videos", "words"
-=======
   add_foreign_key "definitions", "words"
->>>>>>> Definitions
+
 end
