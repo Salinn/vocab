49a3b97f273ef07ead35adc4112647865a617546
Nathan Perry <nbp8557@rit.edu>
2015-10-20 22:46:51 -0400

    Updating the word view to display the sentences associated with the word

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016050821) do
+ActiveRecord::Schema.define(version: 20151021023218) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "class_name", limit: 255
@@ -42,6 +42,24 @@ ActiveRecord::Schema.define(version: 20151016050821) do
 
   add_index "lessons", ["course_id"], name: "index_lessons_on_course_id", using: :btree
 
+  create_table "sentences", force: :cascade do |t|
+    t.string   "word_sentence", limit: 255
+    t.integer  "word_id",       limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "sentences", ["word_id"], name: "index_sentences_on_word_id", using: :btree
+
+  create_table "synonyms", force: :cascade do |t|
+    t.string   "word_synonym", limit: 255
+    t.integer  "word_id",      limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "synonyms", ["word_id"], name: "index_synonyms_on_word_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
@@ -62,6 +80,15 @@ ActiveRecord::Schema.define(version: 20151016050821) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "word_forms", force: :cascade do |t|
+    t.string   "associated_word", limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "word_id",         limit: 4
+  end
+
+  add_index "word_forms", ["word_id"], name: "index_word_forms_on_word_id", using: :btree
+
   create_table "word_roots", force: :cascade do |t|
     t.string   "root_name",       limit: 255
     t.text     "root_definition", limit: 65535
@@ -77,6 +104,16 @@ ActiveRecord::Schema.define(version: 20151016050821) do
   add_index "word_roots_words", ["word_id", "word_root_id"], name: "index_word_roots_words_on_word_id_and_word_root_id", using: :btree
   add_index "word_roots_words", ["word_root_id", "word_id"], name: "index_word_roots_words_on_word_root_id_and_word_id", using: :btree
 
+  create_table "word_videos", force: :cascade do |t|
+    t.string   "video_link",        limit: 255
+    t.string   "video_description", limit: 255
+    t.integer  "word_id",           limit: 4
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
+
+  add_index "word_videos", ["word_id"], name: "index_word_videos_on_word_id", using: :btree
+
   create_table "words", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
@@ -84,4 +121,8 @@ ActiveRecord::Schema.define(version: 20151016050821) do
   end
 
   add_foreign_key "lessons", "courses"
+  add_foreign_key "sentences", "words"
+  add_foreign_key "synonyms", "words"
+  add_foreign_key "word_forms", "words"
+  add_foreign_key "word_videos", "words"
 end
