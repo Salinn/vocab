e511eb046eacee31eceacfeaeb7528c9ce0d98c8
salinn <pmd6624@rit.edu>
2015-10-21 22:30:50 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20151021023218) do
 
   create_table "courses", force: :cascade do |t|
@@ -28,6 +29,14 @@ ActiveRecord::Schema.define(version: 20151021023218) do
   add_index "courses_users", ["course_id", "user_id"], name: "index_courses_users_on_course_id_and_user_id", using: :btree
   add_index "courses_users", ["user_id", "course_id"], name: "index_courses_users_on_user_id_and_course_id", using: :btree
 
+  create_table "definitions", force: :cascade do |t|
+    t.text     "word_definition", limit: 65535
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.integer  "word_id",         limit: 4
+  end
+
+  add_index "definitions", ["word_id"], name: "index_definitions_on_word_id", using: :btree
 
   create_table "lessons", force: :cascade do |t|
     t.string   "lesson_name",       limit: 255
@@ -51,24 +60,6 @@ ActiveRecord::Schema.define(version: 20151021023218) do
 
   add_index "sentences", ["word_id"], name: "index_sentences_on_word_id", using: :btree
 
-  create_table "synonyms", force: :cascade do |t|
-    t.string   "word_synonym", limit: 255
-    t.integer  "word_id",      limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
-
-  add_index "synonyms", ["word_id"], name: "index_synonyms_on_word_id", using: :btree
-
-  create_table "definitions", force: :cascade do |t|
-    t.text     "word_definition", limit: 65535
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.integer  "word_id",         limit: 4
-  end
-
-  add_index "definitions", ["word_id"], name: "index_definitions_on_word_id", using: :btree
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
@@ -88,15 +79,6 @@ ActiveRecord::Schema.define(version: 20151021023218) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "word_forms", force: :cascade do |t|
-    t.string   "associated_word", limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.integer  "word_id",         limit: 4
-  end
-
-  add_index "word_forms", ["word_id"], name: "index_word_forms_on_word_id", using: :btree
 
   create_table "word_roots", force: :cascade do |t|
     t.string   "root_name",       limit: 255
@@ -129,13 +111,8 @@ ActiveRecord::Schema.define(version: 20151021023218) do
     t.datetime "updated_at",             null: false
   end
 
-
+  add_foreign_key "definitions", "words"
   add_foreign_key "lessons", "courses"
   add_foreign_key "sentences", "words"
-  add_foreign_key "synonyms", "words"
-  add_foreign_key "word_forms", "words"
   add_foreign_key "word_videos", "words"
-  add_foreign_key "word_forms", "words"
-  add_foreign_key "word_videos", "words"
-  add_foreign_key "definitions", "words"
 end
