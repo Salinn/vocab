7aabaae96e6745cddd685050bdd8897ca8d8c44e
salinn <pmd6624@rit.edu>
2015-10-22 16:32:56 -0400

    can now pick which defition you want

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,2 +1,9 @@
 module LessonsHelper
+  def lesson_words_link(lesson_word)
+    if lesson_word.word_id.nil?
+      ''
+    else
+      link_to(lesson_word.word.name, lesson_word)
+    end
+  end
 end
