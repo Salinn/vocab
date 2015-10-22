14e2138f1de40385bb429582ff249cea7af6a5b4
salinn <pmd6624@rit.edu>
2015-10-22 17:41:25 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,11 @@ load 'db/seeds/courses.rb'
 
 #Dependincies <-- Order matters
 load 'db/seeds/courses_users.rb' #Depends on Courses and Users
+load 'db/seeds/lessons.rb' #Depends on course
+load 'db/seeds/lesson_words.rb' #Depends on lessons and on words
+load 'db/seeds/definitions.rb' #Depends on Words
+load 'db/seeds/lesson_word_definitions.rb' #Depends on lesson_words and definitions
+
+
 
 #load 'db/seeds/word_roots_words.rb' #Depends on words and word roots
-load 'db/seeds/lessons.rb'
-load 'db/seeds/definitions.rb'
