cb961c91fb14b0228beb4ad13391d7f4e1dd03fb
nbp8557 <nbp8557@rit.edu>
2015-10-30 18:16:39 -0400

    Added relationship information to models

--- app/models/lesson_word.rb
+++ app/models/lesson_word.rb
@@ -3,5 +3,7 @@ class LessonWord < ActiveRecord::Base
   belongs_to :word
   has_many :lesson_word_definitions
   has_many :definitions, through: :lesson_word_definitions
+  has_many :word_videos, through: :lesson_word_videos
   accepts_nested_attributes_for :lesson_word_definitions
+  accepts_nested_attributes_for :lesson_word_videos
 end
