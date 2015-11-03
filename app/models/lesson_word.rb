272c71af0ed3b068d04b263db7b18aacf5ceaa20
Nathan Perry <nbp8557@rit.edu>
2015-11-03 13:44:09 -0500

    Built relationship with Lesson_word and word's sentences

--- app/models/lesson_word.rb
+++ app/models/lesson_word.rb
@@ -8,6 +8,10 @@ class LessonWord < ActiveRecord::Base
   has_many :lesson_word_videos
   has_many :word_videos, through: :lesson_word_videos
 
+  has_many :lesson_word_sentences
+  has_many :sentences, through: :lesson_word_sentences
+
   accepts_nested_attributes_for :lesson_word_definitions
   accepts_nested_attributes_for :lesson_word_videos
+  accepts_nested_attributes_for :lesson_word_sentences
 end
