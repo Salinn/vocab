9a0e74a9f99868cdce81e90263491c83d911382f
salinn <pmd6624@rit.edu>
2015-11-04 14:53:05 -0500

    fixed all tests

--- spec/factories/lessons.rb
+++ spec/factories/lessons.rb
@@ -4,7 +4,6 @@ FactoryGirl.define do
     lesson_points 30
     lesson_start_time DateTime.now
     lesson_end_date DateTime.now + 1.week
-    course_id FactoryGirl.create(:course).id
     penalty 10
   end
 end
