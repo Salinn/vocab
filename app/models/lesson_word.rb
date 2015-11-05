2afcb151cdcb246d39885d27007d5c0eee5582c2
salinn <pmd6624@rit.edu>
2015-11-05 18:15:46 -0500

    added a way to see not selected associations for lesson word

--- app/models/lesson_word.rb
+++ app/models/lesson_word.rb
@@ -22,4 +22,60 @@ class LessonWord < ActiveRecord::Base
   accepts_nested_attributes_for :lesson_word_sentences
   accepts_nested_attributes_for :lesson_word_forms
   accepts_nested_attributes_for :lesson_word_synonyms
+
+
+  def not_word_definitions
+    definitions = []
+    word.definitions.each do |definition|
+      unless lesson_word_definitions.map(&:definition_id).compact.include? definition.id
+        definitions.push definition
+      end
+    end
+
+    definitions
+  end
+
+  def not_word_videos
+    word_videos = []
+    word.word_videos.each do |word_video|
+      unless lesson_word_videos.map(&:word_video_id).compact.include? word_video.id
+        word_videos.push word_video
+      end
+    end
+
+    word_videos
+  end
+
+  def not_word_sentences
+    sentences = []
+    word.sentences.each do |sentence|
+      unless lesson_word_sentences.map(&:sentence_id).compact.include? sentence.id
+        sentences.push sentence
+      end
+    end
+
+    sentences
+  end
+
+  def not_word_forms
+    word_forms = []
+    word.word_forms.each do |word_form|
+      unless lesson_word_forms.map(&:word_form_id).compact.include? word_form.id
+        word_forms.push word_form
+      end
+    end
+
+    word_forms
+  end
+
+  def not_word_synonyms
+    synonyms = []
+    word.synonyms.each do |synonym|
+      unless lesson_word_synonyms.map(&:synonym_id).compact.include? synonym.id
+        synonyms.push synonym
+      end
+    end
+
+    synonyms
+  end
 end
