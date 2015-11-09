62100f1ae129d890dc1cff2621fe1fa498694961
salinn <pmd6624@rit.edu>
2015-11-09 10:24:38 -0500

    fixed the tests

--- spec/views/lessons/new.html.erb_spec.rb
+++ spec/views/lessons/new.html.erb_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe "lessons/new", type: :view do
     assign(:lesson, Lesson.new(
       :lesson_name => "MyString",
       :lesson_points => 1,
-      :course => nil,
       :penalty => 1
     ))
   end
@@ -18,8 +17,6 @@ RSpec.describe "lessons/new", type: :view do
       assert_select "input#lesson_lesson_name[name=?]", "lesson[lesson_name]"
 
       assert_select "input#lesson_lesson_points[name=?]", "lesson[lesson_points]"
-
-      assert_select "input#lesson_course_id[name=?]", "lesson[course_id]"
 
       assert_select "input#lesson_penalty[name=?]", "lesson[penalty]"
     end
