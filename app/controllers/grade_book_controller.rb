b26770df0f21d8f98928aa5e292c8b2b79509eb1
Nathan Perry <nbp8557@rit.edu>
2015-11-19 13:27:46 -0500

    Created example gradebook tables

--- app/controllers/grade_book_controller.rb
+++ app/controllers/grade_book_controller.rb
@@ -1,2 +1,24 @@
 class GradeBookController < ApplicationController
+  before_action :set_grade_book, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @grade_book = Gradebook.all
+  end
+
+  # GET Course/gradebook/
+  # GET Course/gradebook
+  def show
+    @grade_book = Gradebook.all
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_grade_book
+    @grade_book = LessonModule.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def grade_book_params
+    params.require(:grade_book).permit(:modified_grade_value)
+  end
 end
