fd974c78a4c47f457900018d308b9fa2ed193e29
Damas <dam6660@rit.edu>
2015-11-28 22:32:11 -0500

    Added after_create function to lessons, added event relationship to course

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -13,12 +13,16 @@ class Lesson < ActiveRecord::Base
   validates :lesson_start_time, presence: true
   validates :lesson_end_date, presence: true
 
-  after_create :create_modules
+  after_create :create_modules, :create_event
 
   def create_modules
     modules = ['Word Form', 'Definition']
     modules.each do |name|
       LessonModule.create!(name: name, attempts: 3, in_use: true, value_percentage: (100/modules.length).round, lesson_id: id)
     end
+  end
+
+  def create_event
+    Event.create!(title: lesson_name, description: "Due date for lesson", start_time: lesson_start_time , end_time: lesson_end_date)
   end
 end
