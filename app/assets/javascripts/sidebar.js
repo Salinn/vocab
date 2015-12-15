4c330d6284a8fc56ac1708166f57b40c2f97856e
salinn <pmd6624@rit.edu>
2015-12-15 16:52:12 -0500

    updated header

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,5 +1,28 @@
-$(document).ready(function() {
-    $('[data-toggle=offcanvas]').click(function() {
-        $('.row-offcanvas').toggleClass('active');
-    });
+//$(function () {
+//    $('.navbar-toggle').click(function () {
+//        $('.navbar-nav').toggleClass('slide-in');
+//        $('.side-body').toggleClass('body-slide-in');
+//
+//        /// uncomment code for absolute positioning tweek see top comment in css
+//        //$('.absolute-wrapper').toggleClass('slide-in');
+//
+//    });
+//});
+//
+//$("#side-menu-container").toggle(
+//    function () {
+//        $(this).addClass("hidden");
+//    },
+//    function () {
+//        $(this).removeClass("hidden");
+//    }
+//);
+
+$('.toggle').click(function(){
+    if($(".side-menu-container").css("top") == "-160px") {
+        $(".side-menu-container").animate({top: "0px"}, 1000);
+    } else {
+        $(".side-menu-container").animate({top: "-160px"}, 1000);
+    }
+    return false;
 });
