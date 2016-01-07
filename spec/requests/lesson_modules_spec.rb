8111164f08599d264f4b25e4df142d0237fbac79
salinn <pmd6624@rit.edu>
2016-01-06 21:23:20 -0500

    fixed auth tests

--- spec/requests/lesson_modules_spec.rb
+++ spec/requests/lesson_modules_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "LessonModules", type: :request do
+  # Runs before each test.
+  before do
+    # Sign in as a user.
+    sign_in_as_a_valid_user
+  end
+
   describe "GET /lesson_modules" do
     it "works! (now write some real specs)" do
       get lesson_modules_path
