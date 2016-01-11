6f0b382a8ad7c9f12bb9fd561bfc1b59d92c29e0
salinn <pmd6624@rit.edu>
2016-01-10 23:37:32 -0500

    added course view page

--- app/models/gradebook.rb
+++ app/models/gradebook.rb
@@ -1,6 +1,17 @@
 class Gradebook
-  def self.course_grades(user, question)
-    Answer.where(user_id: user.id, question_id: question.id)
+  def self.course_grades(user, lesson)
+    total_correct = 0
+    total_questions = 0
+    total_time = 0
+
+    lesson.lesson_modules.each do |lesson_module|
+      lesson_modules_correct, lesson_modules_question_total, lesson_modules_time = lesson_grades_raw(user, lesson_module)
+      total_correct += lesson_modules_correct
+      total_questions += lesson_modules_question_total
+      total_time += lesson_modules_time
+    end
+    total_correct_percent = ((total_correct.fdiv(total_questions))*100).to_i
+    [total_correct_percent, total_time]
   end
 
   def self.lesson_grades(user, lesson_module)
@@ -11,10 +22,20 @@ class Gradebook
       total_correct += 1 if answer.correct
       total_time += answer.time_to_complete
     end
-    total_correct_percent = ((total_correct.fdiv(lesson_module.questions.length))*100).to_i 
+    total_correct_percent = ((total_correct.fdiv(lesson_module.questions.length))*100).to_i
     [total_correct_percent, total_time]
   end
 
+  def self.lesson_grades_raw(user, lesson_module)
+    total_correct = 0
+    total_time = 0
+    lesson_module.questions.each do |question|
+      answer = lesson_model_last_grade(user, question)
+      total_correct += 1 if answer.correct
+      total_time += answer.time_to_complete
+    end
+    [total_correct, lesson_module.questions.length, total_time]
+  end
 
   def self.lesson_model_grades(user, question)
     Answer.where(user_id: user.id, question_id: question.id)
