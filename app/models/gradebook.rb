6e225bd3102b3f0672574edb81e614e4e995809d
salinn <pmd6624@rit.edu>
2016-01-14 15:47:18 -0500

    refatored gradebook

--- app/models/gradebook.rb
+++ app/models/gradebook.rb
@@ -5,7 +5,7 @@ class Gradebook
     total_time = 0
 
     lesson.lesson_modules.each do |lesson_module|
-      lesson_modules_correct, lesson_modules_question_total, lesson_modules_time = lesson_grades_raw(user, lesson_module)
+      lesson_modules_correct, lesson_modules_question_total, lesson_modules_time = course_lesson_grades(user, lesson_module)
       total_correct += lesson_modules_correct
       total_questions += lesson_modules_question_total
       total_time += lesson_modules_time
@@ -15,52 +15,46 @@ class Gradebook
   end
 
   def self.lesson_grades(user, lesson_module)
-    student_info = []
-    correct = 0
-    time = 0
-    total_correct = 0
-    total_time = 0
-    answers = lesson_model_last_grade(user, lesson_module.question_ids)
-    answers.each do |answer|
-      correct += answer.correct ? 1 : 0
-      time += answer.time_to_complete
-      student_info.push([correct, time])
-      correct = 0
-      time = 0
-    end
-    student_info.each do |answer|
-      total_correct += answer.first
-      total_time += answer.last
-    end
+    answers = lesson_model_grades(user, lesson_module.question_ids)
+    lesson_module_answers = sort_answer_info(answers)
+    total_correct, total_time = get_total_grade_and_time(lesson_module_answers)
     total_correct_percent = ((total_correct.fdiv(lesson_module.questions.length))*100).to_i
     [total_correct_percent, total_time]
   end
 
-  def self.lesson_grades_raw(user, lesson_module)
-    total_correct = 0
-    total_time = 0
-    lesson_module.questions.each do |question|
-      answer = lesson_model_last_grade(user, question)
-      total_correct += 1 if answer.correct
-      total_time += answer.time_to_complete
-    end
+  def self.course_lesson_grades(user, lesson_module)
+    answers = lesson_model_grades(user, lesson_module.question_ids)
+    lesson_module_answers = sort_answer_info(answers)
+    total_correct, total_time = get_total_grade_and_time(lesson_module_answers)
     [total_correct, lesson_module.questions.length, total_time]
   end
 
-  def self.lesson_model_grades(user, question)
+  def self.sort_answer_info(answers)
+    lesson_module_grade_and_time = []
+    answers.each do |answer|
+      correct = 0
+      time = 0
+      correct += answer.correct ? 1 : 0
+      time += answer.time_to_complete
+      lesson_module_grade_and_time.push([correct, time])
+    end
+    lesson_module_grade_and_time
+  end
+
+  def self.get_total_grade_and_time(lesson_module_answers)
+    total_correct, total_time = [0, 0]
+    lesson_module_answers.each do |answer|
+      total_correct += answer.first
+      total_time += answer.last
+    end
+    [total_correct, total_time]
+  end
+
+  def self.lesson_model_grade(user, question)
     Answer.where(user_id: user.id, question_id: question.id)
   end
 
-  def self.lesson_model_last_grade(user, questions)
+  def self.lesson_model_grades(user, questions)
     Answer.where(user_id: user.id, question_id: questions)
   end
 end
-
-
-
-=begin
-  "select * from info WHERE `question_id` IN ('1,2,3,4,5')"
-  sql = "SELECT * FROM answers WHERE `id` IN (#{questions.map{|x| x.id}.join(', ')})"
-  @connection = ActiveRecord::Base.connection
-  @connection.exec_query(sql)
-=end
