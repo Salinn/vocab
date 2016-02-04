4f9258c32f7b642509fe1205fe7b3ed5fc25c46d
salinn <pmd6624@rit.edu>
2016-02-04 09:52:57 -0500

    working login

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -8,17 +8,18 @@ feature 'Signing in' do
 
   scenario 'sign in with incorrect credentials', :js => true do
     visit '/'
-    fill_in 'user_email', :with => 'user@user.com'
-    fill_in 'user_password', :with => 'password'
+    fill_in 'user_email', with: 'user@user.com'
+    fill_in 'user_password', with: 'password'
     click_button 'Log in'
     expect(page).to have_content 'Log in'
   end
 
-#  scenario 'sign in with correct credentials', :js => true do
-#    visit '/'
-#    fill_in 'user_email', :with => 'dam6660@rit.edu'
-#    fill_in 'user_password', :with => 'topsecret'
-#    click_button 'Log in'
-#    expect(page).to have_content 'Classes'
-#  end
+  scenario 'sign in with correct credentials', :js => true do
+    user = FactoryGirl.create(:user)
+    visit '/'
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'Log in'
+    expect(page).to have_content 'Classes'
+  end
 end
