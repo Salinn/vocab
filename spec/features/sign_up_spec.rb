0c404f3aea1f022d0ac75413b2396f53e672095f
Damas <dam6660@rit.edu>
2016-02-16 08:47:02 -0500

    Implemented sign up spec

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
 feature "Sign Up" do
+  scenario 'Sign up', :js => true do
+    visit '/'
+    click_on 'Sign up'
+    expect(page).to have_content 'Sign up'
+    expect(page).to have_content 'Password confirmation'
+
+    fill_in 'user_email', with: 'vocab@email.com'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+
+    click_button 'Sign up'
+
+    expect(page).to have_content 'Classes'
+  end
 
 end
