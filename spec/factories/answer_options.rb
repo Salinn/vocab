4177d7b0777bbc6714e33697d8707f50ad8180eb
salinn <pmd6624@rit.edu>
2016-03-06 19:07:19 -0500

    updated answer controller spec

--- spec/factories/answer_options.rb
+++ spec/factories/answer_options.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :answer_option do
-    question nil
-    lesson_word nil
+    association :lesson_word, factory: :lesson_word_no_call_backs
+    association :question, factory: :question
   end
 end
