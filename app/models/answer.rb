00bbd7eaf0c3887cf195de984de376e837d9a581
salinn <pmd6624@rit.edu>
2016-03-13 20:14:26 -0400

    updated how answers work

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -7,5 +7,12 @@ class Answer < ActiveRecord::Base
   validates :user_id, presence: true
   validates :time_to_complete, numericality: { only_integer: true }
   validates :correct, inclusion: { in: [true, false] }
-  #validates :answer_option_id, presence: true
+  validates :answer_option_id, presence: true
+
+  # validate :not_an_answer_option
+  #
+  # def not_an_answer_option
+  #   @wrong_answers = Answer.where(user_id: self.user_id, question_id: self.question_id).pluck(:answer_option_id)
+  #   errors.add(:answer, 'Already existing answer') if @wrong_answers.include?(answer_option.id)
+  # end
 end
