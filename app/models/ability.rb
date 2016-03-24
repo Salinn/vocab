7d38ec5f84ffcf9705a5ad4f1da1e82729f3370f
Damas <dam6660@rit.edu>
2016-03-24 13:30:31 -0700

    revert to initial ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
       can :manage, :all
     end
 
-    if user.has_role?(:teacher)
+    if Course.with_role(:teacher, user).any?
       can :manage, Definition
       can :manage, Sentence
       can :manage, Synonym
@@ -17,38 +17,32 @@ class Ability
       can :manage, WordRoot
       can :manage, WordForm
       can :manage, WordVideo
-
-      if Course.with_role(:teacher, user).pluck(:id)
-        can :manage, Course
-        can :manage, CourseEmail
-        can :manage, Lesson
-        can :manage, LessonModule
-        can :manage, Synonym
-        can :manage, Event
-        can :manage, Question
-        can :manage, LessonExtension
-        can :manage, LessonWord
-        can :manage, Answer
-      end
+      can :manage, Course
+      can :manage, CourseEmail
+      can :manage, Lesson
+      can :manage, LessonModule
+      can :manage, Synonym
+      can :manage, Event
+      can :manage, Question
+      can :manage, LessonExtension
+      can :manage, LessonWord
+      can :manage, Answer
 
     end
 
-    if user.has_role?(:student)
+    if Course.with_role(:student, user).any?
       can [:index, :show], Word
       can [:index, :show], WordVideo
       can [:index, :show], WordRoot
       can [:index, :show], WordForm
       can [:index, :show], Synonym
       can [:index, :show], Sentence
-
-      if Course.with_role(:student, user).pluck(:id)
-        can [:index, :show], Course
-        can [:index, :show], Lesson
-        can [:index, :show], LessonModule
-        can [:index, :show], Event
-        can [:index, :show], LessonExtension
-        can [:index, :show], Answer
-      end
+      can [:index, :show], Course
+      can [:index, :show], Lesson
+      can [:index, :show], LessonModule
+      can [:index, :show], Event
+      can [:index, :show], LessonExtension
+      can [:index, :show], Answer
     end
 
   end
