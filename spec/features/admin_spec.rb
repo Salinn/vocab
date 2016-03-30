a30fa2532ec4dc9fc07ef78f10526b34dac41b62
Damas <dam6660@rit.edu>
2016-03-30 11:39:49 -0400

    added ids to static sidebar

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 feature 'Admin' do
   scenario 'Log in as admin', :js => true do
-
+    admin = FactoryGirl.create(:admin)
+#    visit '/'
+#    fill_in 'user_email', with: admin.email
+#    fill_in 'user_password', with: admin.password
+#    click_button 'Log in'
   end
 
   scenario 'Add teacher role', :js => true do
