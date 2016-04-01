7bf5daf1eef49a48451966694ac566afeee8bedb
salinn <pmd6624@rit.edu>
2016-03-31 23:16:16 -0400

    working course updates

--- app/controllers/grade_modifiers_controller.rb
+++ app/controllers/grade_modifiers_controller.rb
@@ -29,6 +29,8 @@ class GradeModifiersController < ApplicationController
   end
 
   def create
+    @course = Course.find(params[:course_id])
+    @user_ids = User.with_role(:student, @course).pluck(:id)
     if params[:grade_modifer][:user_id]
       @grade_modifier = GradeModifer.new(grade_modfier_params)
       respond_to do |format|
@@ -40,32 +42,21 @@ class GradeModifiersController < ApplicationController
         end
       end
     elsif params[:grade_modifer][:lesson_id]
-      @course = Course.find(params[:grade_modifer][:course_id])
       params[:grade_modifer][:course_id] = nil
+      lesson_id = params[:grade_modifer][:lesson_id]
       @grade_modifier = GradeModifer.create!(grade_modfier_params)
       grade_modified = Hash.new(0)
-      @users = User.with_role(:student, @course)
       @modified_grade = params[:grade_modifer][:modified_grade_value]
-      modifiers = GradeModifer.where("user_id IN (?) AND lesson_id = (?)", @users.pluck(:id), params[:grade_modifer][:lesson_id])
+      modifiers = GradeModifer.where("user_id IN (?) AND lesson_id = (?)", @user_ids, params[:grade_modifer][:lesson_id])
       modifiers.each { |modifier| grade_modified[modifier.user_id] = modifier.modified_grade_value }
-      @users.each do |user|
-        params[:grade_modifer][:user_id] = user.id
-        params[:grade_modifer][:modified_grade_value] = @modified_grade.to_i + grade_modified[user.id]
-        GradeModifer.create!(grade_modfier_params)
-      end
+      GradeModifer.create_lesson_grade_for_all(@user_ids, lesson_id, @modified_grade, grade_modified)
     else
-      @course = Course.find(params[:grade_modifer][:course_id])
       @grade_modifier = GradeModifer.create!(grade_modfier_params)
       grade_modified = Hash.new(0)
-      @users = User.with_role(:student, @course)
       @modified_grade = params[:grade_modifer][:modified_grade_value]
-      modifiers = GradeModifer.where("user_id IN (?) AND course_id = (?)", @users.pluck(:id), params[:grade_modifer][:course_id])
+      modifiers = GradeModifer.where("user_id IN (?) AND course_id = (?)", @user_ids, params[:grade_modifer][:course_id])
       modifiers.each { |modifier| grade_modified[modifier.user_id] = modifier.modified_grade_value }
-      @users.each do |user|
-        params[:grade_modifer][:user_id] = user.id
-        params[:grade_modifer][:modified_grade_value] = @modified_grade.to_i + grade_modified[user.id]
-        GradeModifer.create!(grade_modfier_params)
-      end
+      GradeModifer.delay.create_course_grade_for_all(@user_ids, @course.id, @modified_grade, grade_modified)
     end
   end
 
@@ -73,9 +64,12 @@ class GradeModifiersController < ApplicationController
   end
 
   def update
+    @course = Course.find(params[:course_id])
+    @user_ids = User.with_role(:student, @course).pluck(:id)
     if params[:grade_modifer][:user_id]
       @grade_modifier = GradeModifer.find(params[:id])
       @old_grade = @grade_modifier.modified_grade_value
+
       respond_to do |format|
         if @grade_modifier.update(grade_modfier_params)
           format.js
@@ -85,28 +79,19 @@ class GradeModifiersController < ApplicationController
         end
       end
     elsif params[:grade_modifer][:lesson_id]
-      @course = Course.find(params[:grade_modifer][:course_id])
       params[:grade_modifer][:course_id] = nil
+      lesson_id = params[:grade_modifer][:lesson_id]
       @grade_modifier = GradeModifer.find(params[:id])
       @old_grade = @grade_modifier.modified_grade_value
       @grade_modifier.update(grade_modfier_params)
-      @users = User.with_role(:student, @course)
       @modified_grade = params[:grade_modifer][:modified_grade_value]
-      modifiers = GradeModifer.where("user_id IN (?) AND lesson_id = (?)", @users.pluck(:id), params[:grade_modifer][:lesson_id])
-      modifiers.each do |modifier|
-        modifier.update(grade_modfier_params)
-      end
+      GradeModifer.update_course_grade_for_all(@user_ids, lesson_id, @modified_grade)
     else
-      @course = Course.find(params[:grade_modifer][:course_id])
       @grade_modifier = GradeModifer.find(params[:id])
       @old_grade = @grade_modifier.modified_grade_value
       @grade_modifier.update(grade_modfier_params)
-      @users = User.with_role(:student, @course)
       @modified_grade = params[:grade_modifer][:modified_grade_value]
-      modifiers = GradeModifer.where("user_id IN (?) AND course_id = (?)", @users.pluck(:id), @course.id)
-      modifiers.each do |modifier|
-        modifier.update(grade_modfier_params)
-      end
+      GradeModifer.update_course_grade_for_all(@user_ids, @course.id, @modified_grade)
     end
   end
 
--- app/models/grade_modifer.rb
+++ app/models/grade_modifer.rb
@@ -4,4 +4,32 @@ class GradeModifer < ActiveRecord::Base
   belongs_to :course
   belongs_to :lesson_module
   #belongs_to :question
+
+  def self.update_course_grade_for_all(user_ids, course_id, updated_grade_value)
+    modifiers = GradeModifer.where("user_id IN (?) AND course_id = (?)", user_ids, course_id)
+    modifiers.each do |modifier|
+      modifier.update(modified_grade_value: updated_grade_value)
+    end
+  end
+
+  def self.update_lesson_grade_for_all(user_ids, lesson_id, updated_grade_value)
+    modifiers = GradeModifer.where("user_id IN (?) AND lesson_id = (?)", user_ids, lesson_id)
+    modifiers.each do |modifier|
+      modifier.update(modified_grade_value: updated_grade_value)
+    end
+  end
+
+  def self.create_course_grade_for_all(user_ids, course_id, grade_modified_value, grade_modified)
+    user_ids.each do |user_id|
+      value = grade_modified[user_id] + grade_modified_value.to_i
+      GradeModifer.create!(user_id: user_id, course_id: course_id, modified_grade_value: value)
+    end
+  end
+
+  def self.create_lesson_grade_for_all(user_ids, lesson_id, grade_modified_value, grade_modified)
+    user_ids.each do |user_id|
+      value = grade_modified[user_id] + grade_modified_value.to_i
+      GradeModifer.create!(user_id: user_id, lesson_id: lesson_id, modified_grade_value: grade_modified_value)
+    end
+  end
 end
