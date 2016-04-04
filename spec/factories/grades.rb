d7bf3738da7f1dca8b5eec60a772f8db4a4c71ba
salinn <pmd6624@rit.edu>
2016-04-04 11:37:51 -0400

    fixed broken test

--- spec/factories/grades.rb
+++ spec/factories/grades.rb
@@ -1,10 +1,9 @@
 FactoryGirl.define do
   factory :grade do
-    user nil
-grade 1
-course nil
-lesson nil
-lesson_module nil
+    association :user, factory: :user
+    grade 100
+    association :course, factory: :course
+    association :lesson, factory: :lesson
+    association :lesson_module, factory: :lesson_module
   end
-
 end
--- spec/views/grades/index.html.erb_spec.rb
+++ spec/views/grades/index.html.erb_spec.rb
@@ -1,31 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "grades/index", type: :view do
+  let(:grade) {FactoryGirl.create(:grade)}
   before(:each) do
     assign(:grades, [
-      Grade.create!(
-        :user => nil,
-        :grade => 1,
-        :course => nil,
-        :lesson => nil,
-        :lesson_module => nil
-      ),
-      Grade.create!(
-        :user => nil,
-        :grade => 1,
-        :course => nil,
-        :lesson => nil,
-        :lesson_module => nil
-      )
+        grade, grade
     ])
   end
 
   it "renders a list of grades" do
     render
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td", :text => grade.user.to_s, :count => 2
+    assert_select "tr>td", :text => 100.to_s, :count => 2
+    assert_select "tr>td", :text => grade.course.to_s, :count => 2
+    assert_select "tr>td", :text => grade.lesson.to_s, :count => 2
+    assert_select "tr>td", :text => grade.lesson_module.to_s, :count => 2
   end
 end
