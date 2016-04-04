2801271aa767007b94b404c54d77af178dbc2936
salinn <pmd6624@rit.edu>
2016-04-04 11:19:57 -0400

    working for heroku seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,39 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #No Dependencies
-startTime = Time.new();
-
+start_time = Time.new
 puts "\nSeeding has begun\n"
 
-#load 'db/seeds/users.rb'
-load 'db/seeds/words.rb'
-#load 'db/seeds/word_roots.rb'
-#load 'db/seeds/courses.rb'
+case Rails.env
+  when 'development'
+    puts 'Seeding in development'
+
+    load 'db/seeds/users.rb'
+    load 'db/seeds/words.rb'
+    load 'db/seeds/word_roots.rb'
+    load 'db/seeds/courses.rb'
 
 
 #Dependincies <-- Order matters
-#load 'db/seeds/lessons.rb' #Depends on course and lesson_modules
-#load 'db/seeds/lesson_words.rb' #Depends on lessons and on words
-#load 'db/seeds/definitions.rb' #Depends on Words
-#load 'db/seeds/sentences.rb'   #Depends on Words
-#load 'db/seeds/synonyms.rb'   #Depends on Words
-#load 'db/seeds/word_videos.rb'   #Depends on Words
-#load 'db/seeds/lesson_word_definitions.rb' #Depends on lesson_words and definitions
-#load 'db/seeds/word_roots_words.rb' #Depends on words and root words
-#load 'db/seeds/lesson_modules.rb'
+    load 'db/seeds/lessons.rb' #Depends on course and lesson_modules
+    load 'db/seeds/lesson_words.rb' #Depends on lessons and on words
+    load 'db/seeds/definitions.rb' #Depends on Words
+    load 'db/seeds/sentences.rb'   #Depends on Words
+    load 'db/seeds/synonyms.rb'   #Depends on Words
+    load 'db/seeds/word_videos.rb'   #Depends on Words
+    load 'db/seeds/lesson_word_definitions.rb' #Depends on lesson_words and definitions
+    load 'db/seeds/word_roots_words.rb' #Depends on words and root words
 
-#load 'db/seeds/full_course_examples.rb'
+    load 'db/seeds/full_course_examples.rb'
 
-#load 'db/seeds/word_roots_words.rb' #Depends on words and word roots
+    load 'db/seeds/word_roots_words.rb' #Depends on words and word roots
+  when 'production'
+    puts 'Seeding in production'
+    load 'db/seeds/users.rb'
+    load 'db/seeds/script.rb'
+end
 
+end_time = Time.new
 
-endTime = Time.new();
-
-puts "\nSeeding has completed in #{(endTime-startTime)} seconds\n"
+puts "\nSeeding has completed in #{(end_time-start_time)} seconds\n"
 
