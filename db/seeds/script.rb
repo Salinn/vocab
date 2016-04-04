0f3fff068d5b0de97c2885c276974b2f82435c49
salinn <pmd6624@rit.edu>
2016-04-04 15:03:28 -0400

    added some stuff to the script

--- db/seeds/script.rb
+++ db/seeds/script.rb
@@ -1,8 +1,10 @@
+require "#{File.dirname(__FILE__)}/../../config/environment"
+
 words = Hash.new{ |h, k| h[k] = [] }
 x=0
 files = Dir["../old_files/*"]
 files.each do |file_name|
-  if !File.directory? file_name
+  unless File.directory? file_name
     File.open(file_name, "r") do |f|
       f.each_line do |line|
         if line =~ /new Array/
@@ -16,30 +18,35 @@ files.each do |file_name|
   end
 end
 words.each do |word|
+  puts word
   word.last.each_with_index do |word_attribute, index|
     case index
       when 0
-        Word.create!(name: word_attribute.split('"')[1])
+        current_word = Word.create!(name: word_attribute.split('"')[1])
       when 1
-        puts '1'
+        Definition.create!(word: current_word, word_definition: word_attribute.split('"')[1])
       when 2
-        puts '1'
+        Sentence.create!(word: current_word, word_sentence: word_attribute.split('"')[1])
       when 3
-        puts '1'
+        word_attribute.split('"')[1].split(',').each do |synonym|
+          Synonym.create!(word: current_word, word_synonym: synonym)
+        end
       when 4
-        puts '1'
+        puts 'No antonyms currently'
       when 5
-        puts '1'
+        WordForm.create!(word: current_word, associated_word: word_attribute.split('"')[1])
       when 6
-        puts '1'
+        WordForm.create!(word: current_word, associated_word: word_attribute.split('"')[1])
       when 7
-        puts '1'
+        WordForm.create!(word: current_word, associated_word: word_attribute.split('"')[1])
       when 8
-        puts '1'
+        WordForm.create!(word: current_word, associated_word: word_attribute.split('"')[1])
       when 9
-        puts '1'
+        WordForm.create!(word: current_word, associated_word: word_attribute.split('"')[1])
       when 10
-        puts '1'
+        word_attribute.split('"')[1].split(',').each do |root|
+          current_word.word_roots.create(root_name: root)
+        end
     end
   end
 end
