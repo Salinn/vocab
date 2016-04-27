0b9157adf8508f8545b93df053ccfccfce9df9a5
salinn <pmd6624@rit.edu>
2016-04-26 21:05:08 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,31 @@
 start_time = Time.new
 puts "\nSeeding has begun\n"
 
-case Rails.env
-  when 'development'
-    puts 'Seeding in development'
-
-    load 'db/seeds/users.rb'
-    load 'db/seeds/words.rb'
-    load 'db/seeds/word_roots.rb'
-    load 'db/seeds/courses.rb'
+if Rails.env == "development"
+  load 'db/seeds/users.rb'
+  load 'db/seeds/words.rb'
+  load 'db/seeds/word_roots.rb'
+  load 'db/seeds/courses.rb'
 
 
 #Dependincies <-- Order matters
-    load 'db/seeds/lessons.rb' #Depends on course and lesson_modules
-    load 'db/seeds/lesson_words.rb' #Depends on lessons and on words
-    load 'db/seeds/definitions.rb' #Depends on Words
-    load 'db/seeds/sentences.rb'   #Depends on Words
-    load 'db/seeds/synonyms.rb'   #Depends on Words
-    load 'db/seeds/word_videos.rb'   #Depends on Words
-    load 'db/seeds/lesson_word_definitions.rb' #Depends on lesson_words and definitions
-    load 'db/seeds/word_roots_words.rb' #Depends on words and root words
+  load 'db/seeds/lessons.rb' #Depends on course and lesson_modules
+  load 'db/seeds/lesson_words.rb' #Depends on lessons and on words
+  load 'db/seeds/definitions.rb' #Depends on Words
+  load 'db/seeds/sentences.rb'   #Depends on Words
+  load 'db/seeds/synonyms.rb'   #Depends on Words
+  load 'db/seeds/word_videos.rb'   #Depends on Words
+  load 'db/seeds/lesson_word_definitions.rb' #Depends on lesson_words and definitions
+  load 'db/seeds/word_roots_words.rb' #Depends on words and root words
 
-    load 'db/seeds/full_course_examples.rb'
+  load 'db/seeds/full_course_examples.rb'
 
-    load 'db/seeds/word_roots_words.rb' #Depends on words and word roots
-  when 'production'
-    puts 'Seeding in production'
-    load 'db/seeds/users.rb'
-    load 'db/seeds/script.rb'
+  load 'db/seeds/word_roots_words.rb' #Depends on words and word roots
+  puts 'Seeding in development'
+elsif Rails.env == "production"
+  load 'db/seeds/users.rb'
+  load 'db/seeds/script.rb'
+  puts 'Seeding in production'
 end
 
 end_time = Time.new
