1caffea5a8334db8305adc2a77558534df626c77
salinn <pmd6624@rit.edu>
2016-05-10 11:09:12 -0400

    fixed dev seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,16 @@
 start_time = Time.new
 puts "\nSeeding has begun\n"
-
+puts Rails.env
 if Rails.env == "development"
   load 'db/seeds/users.rb'
   load 'db/seeds/words.rb'
   load 'db/seeds/word_roots.rb'
   load 'db/seeds/courses.rb'
+  load 'db/seeds/test_words.rb'
 
 
 #Dependincies <-- Order matters
-  load 'db/seeds/lessons.rb' #Depends on course and lesson_modules
+  load 'db/seeds/lessons.rb' #Depends on course
   load 'db/seeds/lesson_words.rb' #Depends on lessons and on words
   load 'db/seeds/definitions.rb' #Depends on Words
   load 'db/seeds/sentences.rb'   #Depends on Words
@@ -17,14 +18,12 @@ if Rails.env == "development"
   load 'db/seeds/word_videos.rb'   #Depends on Words
   load 'db/seeds/lesson_word_definitions.rb' #Depends on lesson_words and definitions
   load 'db/seeds/word_roots_words.rb' #Depends on words and root words
+#load 'db/seeds/lesson_modules.rb'
 
   load 'db/seeds/full_course_examples.rb'
-
-  load 'db/seeds/word_roots_words.rb' #Depends on words and word roots
-  puts 'Seeding in development'
+#load 'db/seeds/word_roots_words.rb' #Depends on words and word roots
 elsif Rails.env == "production"
   load 'db/seeds/users.rb'
-  puts 'Seeding in production'
 end
 
 end_time = Time.new
